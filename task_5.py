2a61f094363a87714333dc7207931e8fd4566f6f
artembigdata <64332640+artembigdata@users.noreply.github.com>
2021-01-11 18:44:26 +0500

    5

--- task_5.py
+++ task_5.py
@@ -3,3 +3,9 @@
 войти четные числа от 100 до 1000 (включая границы). Необходимо получить результат вычисления
 произведения всех элементов списка.Подсказка: использовать функцию reduce().
 """
+from functools import reduce
+
+def my_list(el_1, el_2):
+    return el_1 * el_2
+new_list = [el for el in range(100, 1001, 2)]
+print(f"List\n{new_list}\nMultiplication of numbers\n{reduce(my_list, new_list)}")
